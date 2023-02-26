d417c8b646cd54962950ad315e0c31b02daad102
TunTauk <tauktun628@gmail.com>
2023-02-26 21:03:12 +0630

    add vite

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { MantineProvider } from '@mantine/core'
 import { RouterProvider } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
 import { router } from './routes'
 import { AuthProvider } from './lib/contexts/auth-context'
 import { NotificationsProvider } from '@mantine/notifications'
@@ -26,8 +25,3 @@ root.render(
       </SWRConfig>
    </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
